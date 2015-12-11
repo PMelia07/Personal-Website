9b64eaf5ee02b8a4b3f51394ffc048f7af6ab04f
Patrick Melia <patrickmelia82@gmail.com>
2015-12-11 00:19:19 +0000

    Template
    Template - Commit One

---
+++ src/PersonalBlog/Models/Projects.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonalBlog.Models
+{
+    public class Projects
+    {
+        public int ProjectID { get; set; }
+    }
+}
